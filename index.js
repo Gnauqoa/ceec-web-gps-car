7db3d8cfd3bf97263f0a5576de2e03de98c2c088
Gnauqoa <quanglng2022@gmail.com>
2022-07-06 19:37:30 +0700

    fist commmit

--- index.js
+++ index.js
@@ -2,23 +2,55 @@
 const express = require('express');
 const {Server} = require('socket.io');
 const http = require('http');
-const {MongoClient} = require('mongodb');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 // config database
+const {MongoClient} = require('mongodb');
+
 const uri = "mongodb://localhost:27017";
 const database = new MongoClient(uri);
-const app = express();
-const serverHttp = http.createServer(app);
-const io = new Server(serverHttp);
+
 // io
 io.on('connection', (socket) => {
     console.log('a user connected');
-    //socket.emit('message', 'Welcome to ChatCord!');
-
-    // socket.on('chat message', (msg) => {
-    //     console.log('message: ' + msg);
-    // });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
-
+// request
+let data = [];
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+app.get('/images/car.png', (req, res) => {
+    res.sendFile(__dirname + '/public/images/car.png');
+});
+app.get('/js/main.js', (req, res) => {
+    res.sendFile(__dirname + '/public/js/main.js');
+});
+app.post('/newLatLng', (req,res) => {
+   
+        data.push(req.body);
+        console.log(req.body);
+        io.emit("dataLatLng", [ {
+            lat: 10.762622, 
+            lng: 106.660172,
+            time: "15p" 
+        },
+        {
+            lat: 10.761926, 
+            lng: 106.660418,
+            time: "15p" 
+        }]);
+        res.send("ok")
+  
+  
+});
+server.listen(3000, function () {
+    console.log("Ung dung Node.js dang lang nghe tai dia chi: localhost:3000")
+})
 // database
 async function test() {
 	try {
@@ -44,13 +76,3 @@ async function listDatabases(){
         await database.close();
     }
 };
-// request
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
-app.get('/images/car.png', (req, res) => {
-    res.sendFile(__dirname + '/public/images/car.png');
-});
-const server = app.listen(3000, function () {
-    console.log("Ung dung Node.js dang lang nghe tai dia chi: localhost:3000")
-})
